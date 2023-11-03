29b4eedc4d3d06ad18a088c8edc93fb16fcfeac7
caitlinaliflynn <caitlinaliflynn@gmail.com>
2023-11-03 15:27:37 -0500

    first commit

---
+++ src/main/java/org/launchcode/the_bar_helper/TheBarHelperApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.the_bar_helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBarHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBarHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/the_bar_helper/TheBarHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.the_bar_helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheBarHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
