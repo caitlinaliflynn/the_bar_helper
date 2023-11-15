8ce90aeabe5c79775fe9abcfe989728f7bff6e36
caitlinaliflynn <caitlinaliflynn@gmail.com>
2023-11-15 07:53:19 -0600

    added phone number and address to EmployeeInformation entity class

--- src/main/java/org/launchcode/the_bar_helper/models/EmployeeInformation.java
+++ src/main/java/org/launchcode/the_bar_helper/models/EmployeeInformation.java
@@ -10,12 +10,19 @@ public class EmployeeInformation extends AbstractEntity {
 
     private String dateOfBirth;
 
+    private String phoneNumber;
+
+    private String address;
+
     @OneToOne(mappedBy = "employeeInformation")
     private Employees employees;
 
-    public EmployeeInformation(Position employeePosition, String dateOfBirth) {
+    public EmployeeInformation(Position employeePosition, String dateOfBirth, String phoneNumber, String address, Employees employees) {
         this.employeePosition = employeePosition;
         this.dateOfBirth = dateOfBirth;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.employees = employees;
     }
 
     public EmployeeInformation() {
@@ -36,5 +43,29 @@ public class EmployeeInformation extends AbstractEntity {
 
     public void setDateOfBirth(String dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Employees getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Employees employees) {
+        this.employees = employees;
     }
 }
